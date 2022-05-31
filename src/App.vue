ff4ba23f0f1ba5337d61bf3b6321d93feae9e232
Akash52 <ac8572611@gmail.com>
2022-05-31 15:28:32 +0530

    Setup Pages & Routes

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -31,6 +31,7 @@
 
     <v-main>
       <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -41,8 +42,8 @@ export default {
     drawer: null,
 
     items: [
-      { title: 'Todo', icon: 'mdi-view-dashboard' },
-      { title: 'About', icon: 'mdi-help-box' }
+      { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+      { title: 'About', icon: 'mdi-help-box', to: '/about' }
     ]
   })
 }
