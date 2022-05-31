6474b68b510f5aaf5e2e651d0b78afc9636f91d6
Akash52 <ac8572611@gmail.com>
2022-05-31 15:37:45 +0530

    Get Pages Ready

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About vuetify todo</h1>
+    <p>This is a simple todo app built with vuetify.</p>
+    <p>It uses vue-router and vuetify.</p>
+    <p>It uses vuex and vuex-persistedstate.</p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home pa-6">
+    <h1>Hello Wolrd!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo'
+}
+</script>
+
+<style></style>
