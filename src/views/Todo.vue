fa5581f36f2d2dd84637a71d7fb3596833888e62
Akash52 <ac8572611@gmail.com>
2022-06-07 09:52:17 +0530

    Mark Task as Done

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,13 +2,23 @@
   <div class="home">
     <v-list flat class="pt-0">
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item>
-          <template v-slot:default="{ active }">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.active }"
+        >
+          <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+              <v-checkbox
+                :input-value="task.active"
+                color="primary"
+              ></v-checkbox>
             </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title> {{ task.title }} </v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.active }"
+              >
+                {{ task.title }}
+              </v-list-item-title>
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -29,6 +39,16 @@ export default {
         { id: 2, title: 'Go to work', active: false },
         { id: 3, title: 'Go to bed', active: false }
       ]
+    }
+  },
+  methods: {
+    // eslint-disable-next-line space-before-function-paren
+    doneTask(id) {
+      this.tasks.forEach((task) => {
+        if (task.id === id) {
+          task.active = !task.active
+        }
+      })
     }
   }
 }
