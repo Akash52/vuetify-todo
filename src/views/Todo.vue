9efe4767c5cfbe626b328a5d0463bf12fb77d9eb
Akash52 <ac8572611@gmail.com>
2022-06-08 10:53:37 +0530

    Create “No Tasks” Child Component

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,27 +2,16 @@
   <div class="home">
     <field-add-task />
     <task-list v-if="$store.state.tasks.length" />
-    <div v-else class="no-tasks">
-      <v-icon size="100" color="primary"> mdi-check </v-icon>
-      <div class="text-h5 primary--text">No tasks</div>
-    </div>
+    <no-tasks v-else />
   </div>
 </template>
 
 <script>
 import FieldAddTask from '../components/Todo/FieldAddTask.vue'
+import NoTasks from '../components/Todo/NoTasks.vue'
 import TaskList from '../components/Todo/TaskList.vue'
 export default {
-  components: { FieldAddTask, TaskList },
+  components: { FieldAddTask, TaskList, NoTasks },
   name: 'Home'
 }
 </script>
-
-<style lang="sass">
-.no-tasks
-  position: absolute
-  left: 50%
-  top: 50%
-  transform: translate(-50%, -50%)
-  opacity: 0.5
-</style>
