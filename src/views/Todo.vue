7216dcb4a5dc414b869d6fe34c20a6aee42bc92b
Akash52 <ac8572611@gmail.com>
2022-06-08 10:26:11 +0530

    Create Task List Child Component

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,38 +1,10 @@
 <template>
   <div class="home">
-    <!-- v-field -->
-
     <field-add-task />
-
-    <v-list v-if="$store.state.tasks.length" class="pt-0" flat>
-      <div v-for="task in $store.state.tasks" :key="task.id">
-        <v-list-item
-          @click="$store.commit('doneTask', task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-              >
-                {{ task.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-
-            <v-list-item-action>
-              <v-btn @click.stop="$store.commit('deleteTask', task.id)" icon>
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
+    <task-list
+      v-if="$store.state.tasks.length"
+      @click="$store.commit('doneTask', task.id)"
+    />
     <div v-else class="no-tasks">
       <v-icon size="100" color="primary"> mdi-check </v-icon>
       <div class="text-h5 primary--text">No tasks</div>
@@ -42,8 +14,9 @@
 
 <script>
 import FieldAddTask from '../components/Todo/FieldAddTask.vue'
+import TaskList from '../components/Todo/TaskList.vue'
 export default {
-  components: { FieldAddTask },
+  components: { FieldAddTask, TaskList },
   name: 'Home'
   // eslint-disable-next-line space-before-function-paren
 }
